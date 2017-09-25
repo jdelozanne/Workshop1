4d33f31837571d871083839c26e9486478aa1657
SteefP <s.pelgrom@hotmail.com>
2017-09-25 15:00:06 +0200

    test 6

---
+++ src/kiteshop/controller/ControllerInterface.java
@@ -0,0 +1,16 @@
+package kiteshop.controller;
+
+import kiteshop.pojos.*;
+
+public interface ControllerInterface {
+	
+	void removeKlant(Klant klant);
+	void addKlant(Klant klant);
+	
+	
+	
+	
+	
+	
+
+}
