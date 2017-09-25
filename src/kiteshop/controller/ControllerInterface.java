a29aafaa5872924e2eb965acfaa7f5c730b20ec6
SteefP <s.pelgrom@hotmail.com>
2017-09-25 15:02:57 +0200

    Controller interface toegevoegd vanuit eclipse

--- src/kiteshop/controller/ControllerInterface.java
+++ src/kiteshop/controller/ControllerInterface.java
@@ -1,15 +1,30 @@
 package kiteshop.controller;
 
+import java.util.ArrayList;
+
 import kiteshop.pojos.*;
 
 public interface ControllerInterface {
 	
-	void removeKlant(Klant klant);
-	void addKlant(Klant klant);
-	
-	
-	
-	
+	  void checkLogin();
+	   void addKlant(Klant k);
+	   void removeKlant(Klant k);
+	   void addProduct(Product p);
+	   void removeProduct(Product p);
+	   void addBestelling(Bestelling b);
+	   void removeBestlling(Bestelling b);
+	   void addAccount(Account a);
+	   void removeAccount(Account a);
+	   
+	   //Functies om hele tabbellen op te halen onder, je zult waarschijnlijk ook selecties willen kunnen maken
+	   //maar welke dan precies is nog ter discussie, met sql kun je het zo gek maken als je wilt, dus je zult 
+	   //een selectie moeten maken, weet niet hoeverre dit nu al relecvant is lala lala
+	   ArrayList<Klant> showKlanten();
+	   ArrayList<Product> showProducten();
+	   ArrayList<Bestelling> showBestellingen();
+	   
+	   
+	                   
 	
 	
 
