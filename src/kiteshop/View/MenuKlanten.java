838f9f79b4796c7d254ed32ea4b6f7b2f60d769a
SteefP <s.pelgrom@hotmail.com>
2017-09-26 14:38:43 +0200

    Redelijk end met menu klanten

--- src/kiteshop/View/MenuKlanten.java
+++ src/kiteshop/View/MenuKlanten.java
@@ -42,6 +42,15 @@ public class MenuKlanten {
 			}
 			break;
 		case 2:  
+			klantWijzigenAchternaam();
+			System.out.println("Wilt u nog een klant wijzigen J/N");
+			if(input.next().equalsIgnoreCase("J")){
+				klantWijzigenAchternaam();
+			} else {
+				start();
+			}
+			break;
+		case 3:
 			/* geimplenteerd nu voor zoeken op achternaam (wellicht in de toekomst meer opties) omdat 
 			 * er meerdere klanten kunnen zijn met dezelfde achternaam
 			 */
@@ -53,13 +62,9 @@ public class MenuKlanten {
 				start();
 			}
 			break;
-		case 3:
-
-			printKlanten();
-			verwijderenKlant();
-			break;
 		case 4:
-			printKlanten();
+			controller.showKlanten();
+			start();
 			break;
 		case 5:
 			break; 
@@ -70,6 +75,8 @@ public class MenuKlanten {
 		}
 	}
 
+
+	
 
 	private void nieuweKlantMaken(){
 		
@@ -112,7 +119,7 @@ public class MenuKlanten {
 		String straatnaam = input.next();
 		adres.setStraatnaam(straatnaam);
 		
-		int huisnummer = geefHuisnummer();
+		int huisnummer = geefHuisnummer();  // aparte methode van gemaakt om te checken voor int en evt te herhalen
 		
 		
 		System.out.println("geef toevoeging: ");
@@ -125,6 +132,30 @@ public class MenuKlanten {
 
 	}
 
+	private void klantWijzigenAchternaam() {
+		System.out.println("Geef alstublieft de achternaam van de klant die u wilt wijzigen?");
+		ArrayList<Klant> searchResult = controller.showKlantenAchternaam(input.next());
+
+		System.out.println("De volgende klanten zijn gevonden, geeft u alstublieft het nummer van de klant die u wil wijzigen");
+		for(int i = 0; i < searchResult.size(); i++ ){
+			// arraylist begint op 0, maar ik laat de klant kiezen vanaf 1, moet later wel weer terugrekenen
+			System.out.println(i+1 +" "+ searchResult.get(i));
+		}
+		Klant choosenKlant = searchResult.get(input.nextInt()-1);
+		
+		System.out.println("Wat wilt u wijzigen van de klant? ");
+		System.out.println("Kies 1 voor Voornaam");
+		System.out.println("Kies 2 voor Tussenvoegsel");
+		System.out.println("Kies 3 voor Achternaam");
+		System.out.println("Kies 4 voor E-mail");
+		System.out.println("Kies 5 voor Adres");
+		
+		// We moeten even kijken hoe precies we heir in moeten zijn.
+		
+		
+	}
+
+	
 	private void klantVerwijderenAchterNaam(){
 		System.out.println("Geef alstublieft de achternaam van de klant die u wilt verwijderen?");
 		ArrayList<Klant> searchResult = controller.showKlantenAchternaam(input.next());
@@ -147,7 +178,7 @@ public class MenuKlanten {
 
 	private int geefHuisnummer(){
 		int huisnummer = 0;
-		System.out.println("geef huisnummer: ");  // ik heb hem hier twee pogingen gegeven, anders moet het een aparte methoede worden
+		System.out.println("geef huisnummer: ");  // ik begreep uit een tutorial dat je next int en next niet doorelkaar moet gebruiken, tevens , next intwacht niet, vandaar deze oplossing
 		String tempHuisnummer = input.next();
 		try {
 			huisnummer = Integer.parseInt(tempHuisnummer);
