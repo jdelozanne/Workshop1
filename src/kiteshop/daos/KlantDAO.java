a6961719a28a41fe54c5e1d2f389c52caa244465
SteefP <s.pelgrom@hotmail.com>
2017-09-29 15:29:44 +0200

    add KLant werkt!

--- src/kiteshop/daos/KlantDAO.java
+++ src/kiteshop/daos/KlantDAO.java
@@ -5,10 +5,90 @@
  */
 package kiteshop.daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+import kiteshop.pojos.Account;
+import kiteshop.pojos.Klant;
+
 /**
  *
  * @author julia
  */
-public class KlantDAO {
-    
+public class KlantDAO implements KlantDAOInterface {
+	Connection connection;
+	
+	PreparedStatement statement;
+	ResultSet result;
+
+	public KlantDAO() {
+		this.connection = DBConnect.getConnection();
+	}
+	
+	 
+	/* (non-Javadoc)
+	 * @see kiteshop.daos.KlantDAOInterface#addKlant(kiteshop.pojos.Klant)
+	 */
+	@Override
+	public void addKlant(Klant klant) {
+		try {
+			String sql = "INSERT INTO klant" + "(KlantID, voornaam, tussenvoegsel,   achternaam, emailadres, straatnaam,   huisnummer, toevoeging, postcode,   plaats)" + "values (?,?,? ,?,?,? ,?,?,?, ?)";
+			this.statement = connection.prepareStatement(sql);
+
+			statement.setInt(1, 0);
+			statement.setString(2, klant.getVoornaam());
+			statement.setString(3, klant.getTussenvoegsel());
+			statement.setString(4, klant.getAchternaam());
+			statement.setString(5, klant.getEmail());
+			statement.setString(6, klant.getAdres().getStraatnaam());
+			statement.setInt(7, klant.getAdres().getHuisnummer());
+			statement.setString(8, klant.getAdres().getToevoeging());
+			statement.setString(9, klant.getAdres().getPostcode());
+			statement.setString(10, klant.getAdres().getWoonplaats());
+
+			statement.execute();
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+
+		}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see kiteshop.daos.KlantDAOInterface#removeKlant(kiteshop.pojos.Klant)
+	 */
+	@Override
+	public void removeKlant(Klant klant) {
+      
+
+    }
+	
+	/* (non-Javadoc)
+	 * @see kiteshop.daos.KlantDAOInterface#updateKlant(kiteshop.pojos.Klant)
+	 */
+	@Override
+	public void updateKlant(Klant klant) {
+	      
+
+	    }
+
+	/* (non-Javadoc)
+	 * @see kiteshop.daos.KlantDAOInterface#showKlantenAchternaam(java.lang.String)
+	 */
+	@Override
+	public ArrayList<Klant> showKlantenAchternaam(String achterNaam) {
+     
+        return null;
+    }
+	
+	public static void main (String args[]){
+		new KlantDAO().addKlant(new Klant());
+
+	}
+
 }
