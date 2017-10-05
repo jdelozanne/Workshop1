5d9fd4f0de01e2a24599e16c81bcd36a60886c54
SteefP <s.pelgrom@hotmail.com>
2017-10-05 19:27:50 +0200

    Alles werkt behalve bestelid en bestelling id

--- test/kiteshop/daos/DatabaseTest.java
+++ test/kiteshop/daos/DatabaseTest.java
@@ -29,11 +29,9 @@ public class DatabaseTest {
 
     void initializeDatabase() {
         logger.info("Entering initializeDatabase()");
+        
         // Prepare the SQL statements to drop the DATABASE and recreate it
-
-        String opening1 = "SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0";
-        String opening2 = "SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0; ";
-        String opening3 = "SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES'";
+        //@Julia, dit is allemaal gecopy/paste van de tekst die verschijnt als je forward engineer doet
 
         String dropDatabase = "DROP DATABASE IF EXISTS " + DATABASE;
         String createDatabase = "CREATE DATABASE IF NOT EXISTS " + DATABASE;
@@ -101,25 +99,19 @@ public class DatabaseTest {
                 + "    ON UPDATE NO ACTION)\n"
                 + "ENGINE = InnoDB";
 
-        /*
-      
-        //in neem aan dat dit is om opnieuw met 1 te begginnen met tellen
-        String trigger = "CREATE DEFINER = CURRENT_USER TRIGGER `"+DATABASE+"`.`customer_BEFORE_DELETE` BEFORE DELETE ON `customer` FOR EACH ROW DELETE FROM account WHERE id = OLD.account_id;";
-         */
         try {
 
             Statement stat = connection.createStatement();
-            
-            stat.executeUpdate(opening1);
-            stat.executeUpdate(opening2);
-            stat.executeUpdate(opening3);
+         // Executes the given SQL statement, which may be an INSERT, UPDATE, or DELETE statement or an SQL statement that returns nothing, such as an SQL DDL statement. ExecuteQuery kan niet gebruikt worden voor DDL statements
+          
             stat.executeUpdate(dropDatabase);
-            stat.executeUpdate(createDatabase); // Executes the given SQL statement, which may be an INSERT, UPDATE, or DELETE statement or an SQL statement that returns nothing, such as an SQL DDL statement. ExecuteQuery kan niet gebruikt worden voor DDL statements
+            stat.executeUpdate(createDatabase); 
             stat.executeUpdate(create_account);
             stat.executeUpdate(create_product);
             stat.executeUpdate(create_klant);
-            stat.executeUpdate(create_bestelregel);
             stat.executeUpdate(create_bestelling);
+            stat.executeUpdate(create_bestelregel);
+           
             /*
                 stat.executeUpdate(trigger);
              */
@@ -130,33 +122,16 @@ public class DatabaseTest {
     }
 
     void populateDatabase() {
-        logger.info("Entering populate database");
-        // Generate the required password hashes
-        String pass1 = "welkom";
-        String pass2 = "welkom";
-        String pass3 = "welkom";
-        String pass4 = "geheim";
-        String pass5 = "welkom";
-        String pass6 = "welkom";
-        // Prepare the SQL statements to insert the test data into the DATABASE
-       
-        String insert_account = "INSERT INTO `" + DATABASE + "`.`account` (`gebruikersnaam`, `wachtwoord`) VALUES ('Steef2', 'Bier2');";
-        String insert_product = "INSERT INTO `testkiteshop`.`product` (`idProduct`, `productnaam`, `omschrijving`, `prijs`) VALUES ('1', 'Cabrinha Drifter', 'Cabrinha Drifter 2017 Kite Only Red/Blue - 4,5 meter', '719.00'), ('2', 'Cabrinha Chaos', 'Cabrinha Chaos 2017 Kite Only Yellow/Orange - 5,5 meter', '719.00'),('3', 'Brunotti Virtuoso', 'Brunotti Dimension Kiteboard 136 (model 2015)', '399.00');";
+    	logger.info("Entering populate database");
 
-        /*
-        String insert_klant =
-        String insert_bestelling =
-        String insert_bestelregel =
-        */
-        
-        /*
-        String insert_customer = "INSERT INTO `" + DATABASE + "`.`customer`(`id`,`first_name`,`last_name`,`ln_prefix`,`account_id`) VALUES (1,\"Piet\",\"Pietersen\",null,1), (2,\"Klaas\",\"Klaassen\",null,2),(3,\"Jan\",\"Jansen\",null,3),(4,\"Fred\",\"Boomsma\",null,4),(5,\"Joost\",\"Snel\",null,5)";
-        String insert_address = "INSERT INTO `" + DATABASE + "`.`address`(`id`,`street_name`,`number`,`addition`,`postal_code`,`city`,`customer_id`,`address_type_id`) VALUES (1,\"Postweg\",201,\"h\",\"3781JK\",\"Aalst\",1,1),(2,\"Snelweg\",56,null,\"3922JL\",\"Ee\",2,1),(3,\"Torenstraat\",82,null,\"7620CX\",\"Best\",2,2),(4,\"Valkstraat\",9,\"e\",\"2424DF\",\"Goorle\",2,3),(5,\"Dorpsstraat\",5,null,\"9090NM\",\"Best\",3,1),(6,\"Plein\",45,null,\"2522BH\",\"Oss\",4,1),(7,\"Maduralaan\",23,null,\"8967HJ\",\"Apeldoorn\",5,1)";
-        String insert_order = "INSERT INTO `" + DATABASE + "`.`order`(`id`,`total_price`,`customer_id`,`date`,`order_status_id`) VALUES (1,230.78,1,\"2016-01-01 01:01:01\",3),(2,62.97,1,\"2016-05-02 01:01:01\",3),(3,144.12,1,\"2017-03-02 01:01:01\",2),(4,78.23,2,\"2017-04-08 01:01:01\",3),(5,6.45,3,\"2017-06-28 01:01:01\",1),(6,324.65,3,\"2017-06-07 01:01:01\",3),(7,46.08,3,\"2017-07-07 01:01:01\",2),(8,99.56,4,\"2017-08-17 01:01:01\",1),(9,23.23,5,\"2017-09-13 01:01:01\",3)";
-         = "INSERT INTO `" + DATABASE + "`.`product`(`id`,`name`,`price`,`stock`) VALUES (1,\"Goudse belegen kaas\",12.90,134),(2,\"Goudse extra belegen kaas\",14.70,239),(3,\"Leidse oude kaas\",14.65,89),(4,\"Schimmelkaas\",11.74,256),(5,\"Leidse jonge kaas\",11.24,122),(6,\"Boeren jonge kaas\",12.57,85)";
-        String insert_order_item = "INSERT INTO `" + DATABASE + "`.`order_item`(`id`,`order_id`,`product_id`,`amount`,`subtotal`) VALUES (1,1,6,23,254.12),(2,1,1,26,345.20),(3,1,2,2,24.14),(4,2,1,25,289.89),(5,3,4,2,34.89),(6,4,2,13,156.76),(7,4,5,2,23.78),(8,5,2,2,21.34),(9,6,1,3,35.31),(10,6,3,1,11.23),(11,7,6,1,14.23),(12,7,2,3,31.87),(13,8,4,23,167.32),(14,9,1,1,11.34),(15,9,2,2,22.41)";
-        logger.info("Try connection for pop database");
-        */
+    	// Prepare the SQL statements to insert the test data into the DATABASE
+    	String insert_account = "INSERT INTO `" + DATABASE + "`.`account` (`gebruikersnaam`, `wachtwoord`) VALUES ('Steef2', 'Bier2');";
+    	String insert_product = "INSERT INTO `" + DATABASE + "`.`product` (`idProduct`, `productnaam`, `omschrijving`, `prijs`) VALUES ('1', 'Cabrinha Drifter', 'Cabrinha Drifter 2017 Kite Only Red/Blue - 4,5 meter', '719.00'), ('2', 'Cabrinha Chaos', 'Cabrinha Chaos 2017 Kite Only Yellow/Orange - 5,5 meter', '719.00'),('3', 'Brunotti Virtuoso', 'Brunotti Dimension Kiteboard 136 (model 2015)', '399.00');";
+    	String insert_klant = "INSERT INTO `" + DATABASE + "`.`klant` (`KlantID`, `voornaam`, `achternaam`, `emailadres`, `straatnaam`, `huisnummer`, `postcode`, `plaats`, `telefoonnummer`) VALUES ('1', 'Steef', 'Pelgrom', 'stevey@hotmail.com', 'Willem de Zuiperlaan', '38', '5034KL', 'Tilburg', '06-56847965');";
+    	String insert_bestelling = "INSERT INTO `" + DATABASE + "`.`bestelling` (`bestellingID`, `klantID`) VALUES ('1', '1');";
+    	String insert_bestelregel = "INSERT INTO `" + DATABASE + "`.`bestel_regel` (`bestel_regelID`, `aantal`, `Bestelling_bestellingID`, `product_idProduct`) VALUES ('1', '4', '1', '2'), ('2', '1', '1', '2');";
+    	
+  
         try {
 
             // Execute the SQL statements to insert the test data into the DATABASE
@@ -164,13 +139,10 @@ public class DatabaseTest {
             
             stat.executeUpdate(insert_account);
             stat.executeUpdate(insert_product);
-            /*
-            stat.executeUpdate(insert_customer);
-            stat.executeUpdate(insert_address);
-            stat.executeUpdate(insert_order);
-            stat.executeUpdate(insert_product);
-            stat.executeUpdate(insert_order_item);
-            */
+            stat.executeUpdate(insert_klant);
+            stat.executeUpdate(insert_bestelling);
+            stat.executeUpdate(insert_bestelregel);
+          
 
         } catch (SQLException ex) {
             logger.info("SQLException" + ex);
