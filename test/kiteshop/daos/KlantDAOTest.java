b6f826b4b72ab1f2af9bf6bac94799b37ba190c9
SteefP <s.pelgrom@hotmail.com>
2017-10-06 16:26:51 +0200

    test

--- test/kiteshop/daos/KlantDAOTest.java
+++ test/kiteshop/daos/KlantDAOTest.java
@@ -6,7 +6,7 @@
 package kiteshop.daos;
 
 import java.util.ArrayList;
-import kiteshop.pojos.*;
+import kiteshop.pojos.Klant;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -23,23 +23,6 @@ public class KlantDAOTest {
     public KlantDAOTest() {
     }
     
-    @Before
-    public void initializeDatabase() {        
-        new DatabaseTest().initializeDatabase();
-        
-        
-        
-      
-       
-        
-                
-        
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-    
     @BeforeClass
     public static void setUpClass() {
     }
@@ -47,125 +30,13 @@ public class KlantDAOTest {
     @AfterClass
     public static void tearDownClass() {
     }
-
-    /**
-     * Test of createKlant method, of class KlantDAO.
-     */
-    @Test
-    public void testAddKlant() {
-        System.out.println("addKlant");
-        
-               
-        
-        Klant klant = new Klant();
-        klant.setVoornaam("Steef");
-        klant.setTussenvoegsel("v");
-        klant.setAchternaam("Pelgrom");
-        Adres adres = new Adres();
-        adres.setWoonplaats("Tilburg");
-        adres.setStraatnaam("Hendrik van Tulderstraat");
-        adres.setPostcode("5046NC");
-        adres.setHuisnummer(17);
-        adres.setToevoeging("a");
-        adres.setEmail("s.pelgrom@hotmail.com");
-        adres.setTelefoon(06-26634587);
-        
-        
-        
-        klant.setAdres(adres);
-        
-        KlantDAO instance = new KlantDAO();
-        instance.createKlant(klant);
-        
-        String expResult = klant.toString();
-        String result = instance.readSelectedKlantenAchternaam("Pelgrom").get(0).toString();
-        
-        System.out.println("Expected Result: " +expResult);
-        System.out.println("Reasult" +result);
-        
-        assertEquals(expResult, result);
-        
+    
+    @Before
+    public void setUp() {
     }
-
-    /**
-     * Test of deleteKlant method, of class KlantDAO.
-     */
-    @Test
-    public void testDeleteKlant() {
-        System.out.println("deleteKlant");
-        Klant klant = null;
-        KlantDAO instance = new KlantDAO();
-        instance.deleteKlant(klant);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateKlant method, of class KlantDAO.
-     */
-    @Test
-    public void testUpdateKlant() {
-        System.out.println("updateKlant");
-        Klant klant = null;
-        KlantDAO instance = new KlantDAO();
-        instance.updateKlant(klant);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Nog niet klaar!!
-     */
-    @Test
-    public void testShowKlantenAchternaam() {
-        System.out.println("showKlantenAchternaam");
-        
-        String achterNaam = "Pelegrino";
-        Klant klant = new Klant();
-        klant.setVoornaam("Stevey");
-        klant.setTussenvoegsel("v");
-        klant.setAchternaam(achterNaam);
-        Adres adres = new Adres();
-        adres.setWoonplaats("Tilburg");
-        adres.setStraatnaam("Hendrik van Tulderstraat");
-        adres.setPostcode("5046NC");
-        adres.setHuisnummer(17);
-        adres.setToevoeging("a");
-        adres.setEmail("s.pelgrom@hotmail.com");
-        adres.setTelefoon(06-26634587);
-        
-        klant.setAdres(adres);
-        
-        
-        
-        
-        ArrayList<Klant> klantenlijst = new ArrayList<Klant>();
-        klantenlijst.add(klant);
-        
-        
-        KlantDAO instance = new KlantDAO();
-        instance.createKlant(klant);
-        ArrayList<Klant> expResult = klantenlijst;
-        ArrayList<Klant> result = instance.readSelectedKlantenAchternaam(achterNaam);
-        System.out.println(expResult);
-        System.out.println(result);
-        
-       
-        
-        assertEquals(expResult.toString(), result.toString());
-        
-    }
-
-    /**
-     * Test of main method, of class KlantDAO.
-     */
-    @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        KlantDAO.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    
+    @After
+    public void tearDown() {
     }
 
     /**
@@ -192,6 +63,32 @@ public class KlantDAOTest {
         Klant expResult = null;
         Klant result = instance.readKlant(achternaam);
         assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of updateKlant method, of class KlantDAO.
+     */
+    @Test
+    public void testUpdateKlant() {
+        System.out.println("updateKlant");
+        Klant klant = null;
+        KlantDAO instance = new KlantDAO();
+        instance.updateKlant(klant);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deleteKlant method, of class KlantDAO.
+     */
+    @Test
+    public void testDeleteKlant() {
+        System.out.println("deleteKlant");
+        Klant klant = null;
+        KlantDAO instance = new KlantDAO();
+        instance.deleteKlant(klant);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -232,6 +129,18 @@ public class KlantDAOTest {
         ArrayList<Klant> klanten = null;
         KlantDAO instance = new KlantDAO();
         instance.displayKlanten(klanten);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class KlantDAO.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        KlantDAO.main(args);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
